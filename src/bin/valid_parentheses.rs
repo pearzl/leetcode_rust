1959b94f53aeb5f60cbe2da05b8161bf4c1e02ce
pearzl <302669696@qq.com>
2019-07-11 02:41:52 +0800

    20

---
+++ src/bin/valid_parentheses.rs
@@ -0,0 +1,52 @@
+//20
+
+fn main() {
+    assert_eq!(is_valid("()".to_string()), true);
+    assert_eq!(is_valid("()[]{}".to_string()), true);
+    assert_eq!(is_valid("(]".to_string()), false);
+    assert_eq!(is_valid("([)]".to_string()), false);
+    assert_eq!(is_valid("{[]}".to_string()), true);
+}
+
+pub fn is_valid(s: String) -> bool {
+    let mut v = vec![];
+    for &c in s.as_bytes() {
+        if c == b'{' || c == b'[' || c == b'(' {
+            v.push(c);
+        } else if c == b'}' {
+            match v.pop() {
+                Some(f) => {
+                    if f != b'{' {
+                        return false;
+                    }
+                }
+                None => return false,
+            }
+        } else if c == b']' {
+            match v.pop() {
+                Some(f) => {
+                    if f != b'[' {
+                        return false;
+                    }
+                }
+                None => return false,
+            }
+        } else if c == b')' {
+            match v.pop() {
+                Some(f) => {
+                    if f != b'(' {
+                        return false;
+                    }
+                }
+                None => return false,
+            }
+        } else {
+            panic!("invalid input");
+        }
+    }
+    if v.len() > 0 {
+        return false;
+    } else {
+        return true;
+    }
+}
