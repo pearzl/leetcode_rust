ad68a38fef4b0166f58c3cfd6323774aa123a3e0
pearzl <302669696@qq.com>
2019-07-11 02:41:26 +0800

    17

---
+++ src/bin/letter_combinations_of_a_phone_number.rs
@@ -0,0 +1,42 @@
+// 17
+
+fn main() {
+    assert_eq!(
+        vec!["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"],
+        letter_combinations("234".to_string())
+    );
+}
+
+pub fn letter_combinations(digits: String) -> Vec<String> {
+    let map = vec![
+        vec![b'a', b'b', b'c'],
+        vec![b'd', b'e', b'f'],
+        vec![b'g', b'h', b'i'],
+        vec![b'j', b'k', b'l'],
+        vec![b'm', b'n', b'o'],
+        vec![b'p', b'q', b'r', b's'],
+        vec![b't', b'u', b'v'],
+        vec![b'w', b'x', b'y', b'z'],
+    ];
+    let mut ret = vec![];
+    for d in digits.as_bytes() {
+        if *d < b'2' || *d > b'9' {
+            return ret;
+        }
+        let k = *d - b'2';
+        if ret.len() == 0 {
+            for i in map.get(k as usize).unwrap().iter() {
+                ret.push((*i as char).to_string());
+            }
+            continue;
+        }
+        let mut new_ret = vec![];
+        for i in ret.iter() {
+            for j in map.get(k as usize).unwrap().iter() {
+                new_ret.push(format!("{}{}", i, *j as char));
+            }
+        }
+        ret = new_ret;
+    }
+    ret
+}
