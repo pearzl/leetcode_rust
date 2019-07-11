c4dd01cf66945412565e0df813aef50f73344084
pearzl <302669696@qq.com>
2019-07-11 08:42:05 +0800

    27

---
+++ src/bin/remove_element.rs
@@ -0,0 +1,31 @@
+//27
+
+pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+    let mut a = 0;
+    let mut b = 0;
+    while a < nums.len() {
+        println!("{}--{}", a, b);
+        while nums[a] == val {
+            a += 1;
+            if a >= nums.len() {
+                return b as i32;
+            }
+        }
+        nums[b] = nums[a];
+        a += 1;
+        b += 1;
+    }
+    return b as i32;
+}
+
+fn main() {
+    let mut v = vec![3,2,2,3];
+    let n = remove_element(&mut v, 3);
+    assert_eq!(2, n);
+    assert_eq!(&v[0..n as usize], &vec![2,2][..]);
+
+    let mut v = vec![0,1,2,2,3,0,4,2];
+    let n = remove_element(&mut v, 2);
+    assert_eq!(5, n);
+    assert_eq!(&v[0..n as usize], &vec![0, 1, 3, 0, 4][..]);
+}
