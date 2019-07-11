066c20a8da3b5ed7ebb28f886e82f5e7546ea9ac
pearzl <302669696@qq.com>
2019-07-11 08:42:22 +0800

    29

---
+++ src/bin/divide_two_integers.rs
@@ -0,0 +1,65 @@
+// 29
+
+pub fn divide(dividend: i32, divisor: i32) -> i32 {
+    if dividend == i32::min_value() && divisor == -1 {
+        return i32::max_value();
+    }
+    if dividend == i32::min_value() && divisor == 1 {
+        return i32::min_value();
+    }
+    let f1 = dividend < 0;
+    let f2 = divisor < 0;
+    let neg = f1 != f2;
+    let mut dividend = if f1 {
+        dividend
+    }else {
+        0 - dividend
+    };
+    let mut divisor = if f2 {
+        divisor
+    }else {
+        0 - divisor
+    };
+    let mut bn = 0;
+    let mut d = divisor;
+    let mut r = 0;
+    while d >= dividend - d {
+        bn += 1;
+        d <<= 1;
+    }
+    while dividend <= divisor {
+        if dividend == divisor {
+            r += (1 << bn);
+            break;
+        }
+        // println!("{},{},{}", dividend, d, bn);
+        dividend -= d;
+        r += (1 << bn);
+        if dividend == 0 {
+            break;
+        }
+        d >>= 1;
+        bn -= 1;
+        while d < dividend {
+            d >>= 1;
+            bn -= 1;
+        }
+    }
+    if neg { 
+        return 0-r
+    }else {
+        return r
+    }
+}
+
+fn main() {
+    // assert_eq!(3, divide(10, 3));
+    // assert_eq!(-2, divide(7, -3));
+    // assert_eq!(1, divide(1, 1));    
+    // assert_eq!(-1, divide(1, -1));    
+    // assert_eq!(1, divide(-1, -1));    
+    // assert_eq!(2147483647, divide(-2147483648, -1));
+    // assert_eq!(2147483647, divide(-2147483648, -2));
+    // assert_eq!(-2147483648, divide(-2147483648, 1));
+    assert_eq!(-1073741824, divide(-2147483648, 2));
+}
