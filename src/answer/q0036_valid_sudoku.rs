a715d7d8ba787b59e36519a9a0c4463d13eff344
pearzl <302669696@qq.com>
2019-07-12 13:23:38 +0800

    36

---
+++ src/answer/q0036_valid_sudoku.rs
@@ -0,0 +1,101 @@
+// q0036_valid_sudoku
+
+struct Solution;
+
+impl Solution {
+    pub fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
+        let mut line_checker: [[bool; 9]; 9] = [[false; 9]; 9];
+        let mut row_checker: [[bool; 9]; 9] = [[false; 9]; 9];
+        let mut block_checker: [[bool; 9]; 9] = [[false; 9]; 9];
+        for (i, line) in board.iter().enumerate() {
+            for (j, c) in line.iter().enumerate() {
+                if *c == '.' {
+                    continue;
+                }
+                let c = (*c as u8 - b'1') as usize;
+                // if i==4 && c == 4 {
+                //     // println!("{}---{}", i, j);
+                // }
+                if line_checker[i][c] {
+                    // println!("linec: {}-{}", i, c);
+                    return false;
+                } else {
+                    line_checker[i][c] = true;
+                }
+                if row_checker[j][c] {
+                    // println!("rowc: {}-{}", j, c);
+                    return false;
+                } else {
+                    row_checker[j][c] = true;
+                }
+                let n = (i / 3) * 3 + (j / 3);
+                if block_checker[n][c] {
+                    // println!("blockc: {}-{}", n, c);
+                    return false;
+                } else {
+                    block_checker[n][c] = true;
+                }
+            }
+        }
+        return true;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    fn build(s: [[&str; 9]; 9]) -> Vec<Vec<char>> {
+        let mut ret = vec![];
+        for line in s.iter() {
+            ret.push(line.iter().map(|c| c.chars().next().unwrap()).collect());
+        }
+        ret
+    }
+
+    #[test]
+    fn it_works() {
+        // assert_eq!(
+        //     true,
+        //     Solution::is_valid_sudoku(build([
+        //         ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+        //         ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+        //         [".", "9", "8", ".", ".", ".", ".", "6", "."],
+        //         ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+        //         ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+        //         ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+        //         [".", "6", ".", ".", ".", ".", "2", "8", "."],
+        //         [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+        //         [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+        //     ]))
+        // );
+        // assert_eq!(
+        //     false,
+        //     Solution::is_valid_sudoku(build([
+        //         ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+        //         ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+        //         [".", "9", "8", ".", ".", ".", ".", "6", "."],
+        //         ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+        //         ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+        //         ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+        //         [".", "6", ".", ".", ".", ".", "2", "8", "."],
+        //         [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+        //         [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+        //     ]))
+        // );
+        assert_eq!(
+            true,
+            Solution::is_valid_sudoku(build([
+                [".", "8", "7", "6", "5", "4", "3", "2", "1"],
+                ["2", ".", ".", ".", ".", ".", ".", ".", "."],
+                ["3", ".", ".", ".", ".", ".", ".", ".", "."],
+                ["4", ".", ".", ".", ".", ".", ".", ".", "."],
+                ["5", ".", ".", ".", ".", ".", ".", ".", "."],
+                ["6", ".", ".", ".", ".", ".", ".", ".", "."],
+                ["7", ".", ".", ".", ".", ".", ".", ".", "."],
+                ["8", ".", ".", ".", ".", ".", ".", ".", "."],
+                ["9", ".", ".", ".", ".", ".", ".", ".", "."]
+            ]))
+        );
+    }
+}
