8d2b3f0de6e17a931ef5d22a2ca7ebd1a12fcb27
pearzl <302669696@qq.com>
2019-07-16 06:17:58 +0800

    fmt

--- src/answer/q0060_permutation_sequence.rs
+++ src/answer/q0060_permutation_sequence.rs
@@ -1,22 +1,20 @@
-// q0060_permutation_sequence 
-
+// q0060_permutation_sequence
 
 struct Solution;
 
-const FACTORIAL: [i32;9] = [1, 2, 6, 24, 120, 720, 5040, 40320, 362880];
-const NUMS: [u8;9] = [b'1',b'2',b'3',b'4',b'5',b'6',b'7',b'8',b'9'];
+const FACTORIAL: [i32; 9] = [1, 2, 6, 24, 120, 720, 5040, 40320, 362880];
+const NUMS: [u8; 9] = [b'1', b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9'];
 
 impl Solution {
-
     pub fn get_permutation(n: i32, k: i32) -> String {
         let mut picked_num = vec![];
         let mut k = k - 1;
         let n = n as usize;
-        for round in (0..n-1).rev() {
+        for round in (0..n - 1).rev() {
             let index = k / FACTORIAL[round];
             // println!("index{}  k{} round{}", index, k, round);
             Solution::pick_num(&mut picked_num, index, n);
-            k %= FACTORIAL[round];            
+            k %= FACTORIAL[round];
         }
         Solution::pick_num(&mut picked_num, 0, n);
         String::from_utf8(picked_num).unwrap()
@@ -31,7 +29,7 @@ impl Solution {
                 continue;
             }
             if count == index {
-        // println!("  picked {}", *num);
+                // println!("  picked {}", *num);
                 picked_num.push(*num);
                 break;
             }
@@ -40,24 +38,21 @@ impl Solution {
     }
 }
 
-
-
 #[cfg(test)]
 mod tests {
     use super::Solution;
 
     #[test]
     fn it_works() {
-        assert_eq!( String::from("1"), Solution::get_permutation(1, 1));
-        assert_eq!( String::from("12"), Solution::get_permutation(2, 1));
-        assert_eq!( String::from("21"), Solution::get_permutation(2, 2));
-        assert_eq!( String::from("123"), Solution::get_permutation(3, 1));
-        assert_eq!( String::from("132"), Solution::get_permutation(3, 2));
-        assert_eq!( String::from("213"), Solution::get_permutation(3, 3));
-        assert_eq!( String::from("231"), Solution::get_permutation(3, 4));
-        assert_eq!( String::from("312"), Solution::get_permutation(3, 5));
-        assert_eq!( String::from("321"), Solution::get_permutation(3, 6));
+        assert_eq!(String::from("1"), Solution::get_permutation(1, 1));
+        assert_eq!(String::from("12"), Solution::get_permutation(2, 1));
+        assert_eq!(String::from("21"), Solution::get_permutation(2, 2));
+        assert_eq!(String::from("123"), Solution::get_permutation(3, 1));
+        assert_eq!(String::from("132"), Solution::get_permutation(3, 2));
+        assert_eq!(String::from("213"), Solution::get_permutation(3, 3));
+        assert_eq!(String::from("231"), Solution::get_permutation(3, 4));
+        assert_eq!(String::from("312"), Solution::get_permutation(3, 5));
+        assert_eq!(String::from("321"), Solution::get_permutation(3, 6));
     }
 
 }
-
