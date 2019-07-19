57ea3754f25e38423a4793fbad8bbd53ccf4106e
pearzl <302669696@qq.com>
2019-07-20 00:38:03 +0800

    treenode

--- src/util.rs
+++ src/util.rs
@@ -35,36 +35,70 @@ impl ListNode {
     }
 }
 
-use std::rc::Rc;
 use std::cell::RefCell;
+use std::rc::Rc;
 // Definition for a binary tree node.
 #[derive(Debug, PartialEq, Eq)]
 pub struct TreeNode {
-  pub val: i32,
-  pub left: Option<Rc<RefCell<TreeNode>>>,
-  pub right: Option<Rc<RefCell<TreeNode>>>,
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
 }
+
+use std::collections::VecDeque;
 
 impl TreeNode {
-  #[inline]
-  pub fn new(val: i32) -> Self {
-    TreeNode {
-      val,
-      left: None,
-      right: None
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
     }
-  }
+
+    pub fn build(v: Vec<Option<i32>>) -> Option<Rc<RefCell<TreeNode>>> {
+        if v.len() == 0 {
+            return None;
+        }
+        let mut v_iter = v.into_iter();
+        let head_v = v_iter.next().unwrap().unwrap();
+        let head = Rc::new(RefCell::new(TreeNode::new(head_v)));
+        let mut tmp = VecDeque::new();
+        tmp.push_back(Rc::clone(&head));
+        while let Some(node) = tmp.pop_front() {
+            if let Some(lv) = v_iter.next() {
+                match lv {
+                    Some(val) => {
+                        let ln = Rc::new(RefCell::new(TreeNode::new(val)));
+                        tmp.push_back(Rc::clone(&ln));
+                        node.borrow_mut().left = Some(ln);
+                    }
+                    None => {
+                        node.borrow_mut().left = None;
+                    }
+                }
+            } else {
+                break;
+            }
+            if let Some(rv) = v_iter.next() {
+                match rv {
+                    Some(val) => {
+                        let rn = Rc::new(RefCell::new(TreeNode::new(val)));
+                        tmp.push_back(Rc::clone(&rn));
+                        node.borrow_mut().right = Some(rn);
+                    }
+                    None => {
+                        node.borrow_mut().right = None;
+                    }
+                }
+            } else {
+                break;
+            }
+        }
+        Some(head)
+    }
 }
-
-
-
-
-
-
-
-
-
-
 
 use std::collections::HashSet;
 
@@ -104,5 +138,32 @@ pub fn build_sudo(s: [[&str; 9]; 9]) -> Vec<Vec<char>> {
 pub fn print_sudo<T: std::fmt::Debug>(sudo: Vec<Vec<T>>) {
     for v in sudo.iter() {
         println!("{:?}", v);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::TreeNode;
+    use std::cell::RefCell;
+    use std::rc::Rc;
+
+    #[test]
+    fn build_tree_node() {
+        assert_eq!(
+            TreeNode::build(vec![Some(1), None, Some(2), Some(3)]),
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: 1,
+                left: None,
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 3,
+                        left: None,
+                        right: None
+                    }))),
+                    right: None
+                })))
+            })))
+        );
     }
 }
