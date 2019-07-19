307d9b2fb52d414367dee7bf71a3d4134eb0b272
pearzl <302669696@qq.com>
2019-07-19 22:04:38 +0800

    TreeNode

--- src/util.rs
+++ src/util.rs
@@ -35,6 +35,37 @@ impl ListNode {
     }
 }
 
+use std::rc::Rc;
+use std::cell::RefCell;
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+  pub val: i32,
+  pub left: Option<Rc<RefCell<TreeNode>>>,
+  pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+  #[inline]
+  pub fn new(val: i32) -> Self {
+    TreeNode {
+      val,
+      left: None,
+      right: None
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
 use std::collections::HashSet;
 
 pub fn vec_2_set<T: Clone + std::hash::Hash + Eq>(v: Vec<T>) -> HashSet<T> {
