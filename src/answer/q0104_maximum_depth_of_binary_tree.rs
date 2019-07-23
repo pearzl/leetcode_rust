a0136b234adc53ff3bf6c80171436a7a1bf16917
pearzl <302669696@qq.com>
2019-07-23 14:02:04 +0800

    104

---
+++ src/answer/q0104_maximum_depth_of_binary_tree.rs
@@ -0,0 +1,49 @@
+// q0104_maximum_depth_of_binary_tree
+
+struct Solution;
+
+use crate::util::TreeNode;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        if let Some(rrc) = root {
+            match (&rrc.borrow().left, &rrc.borrow().right) {
+                (Some(ln), Some(rn)) => {
+                    let d1 = Solution::max_depth(Some(Rc::clone(ln)));
+                    let d2 = Solution::max_depth(Some(Rc::clone(rn)));
+                    return 1 + d1.max(d2);
+                }
+                (Some(ln), None) => return 1 + Solution::max_depth(Some(Rc::clone(ln))),
+                (None, Some(rn)) => return 1 + Solution::max_depth(Some(Rc::clone(rn))),
+                (None, None) => return 1,
+            }
+        } else {
+            return 0;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    use crate::util::TreeNode;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(
+            3,
+            Solution::max_depth(TreeNode::build(vec![
+                Some(3),
+                Some(9),
+                Some(20),
+                None,
+                None,
+                Some(15),
+                Some(7)
+            ]))
+        );
+    }
+}
