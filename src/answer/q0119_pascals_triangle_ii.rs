2c6ae7f470d3ed5243a7d52507436cc054181273
pearzl <302669696@qq.com>
2019-08-14 19:09:10 +0800

    119

--- src/answer/q0119_pascals_triangle_ii.rs
+++ src/answer/q0119_pascals_triangle_ii.rs
@@ -1,11 +1,10 @@
-// q0119_pascals_triangle_ii 
-
+// q0119_pascals_triangle_ii
 
 struct Solution;
 
 impl Solution {
     pub fn get_row(row_index: i32) -> Vec<i32> {
-        let mut ret = Solution::generate(row_index+1);
+        let mut ret = Solution::generate(row_index + 1);
         ret.pop().unwrap()
     }
 
@@ -20,17 +19,16 @@ impl Solution {
             let last_line = ret.last().unwrap();
             let mut tv = Vec::with_capacity(line);
             tv.push(last_line[0]);
-            for i in 1..line-1 {
-                let t = last_line[i-1] + last_line[i];
+            for i in 1..line - 1 {
+                let t = last_line[i - 1] + last_line[i];
                 tv.push(t);
             }
-            tv.push(last_line[line-2]);
+            tv.push(last_line[line - 2]);
             ret.push(tv);
         }
         ret
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -38,7 +36,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        assert_eq!( Solution::get_row(3) , vec![1,3,3,1]);
+        assert_eq!(Solution::get_row(3), vec![1, 3, 3, 1]);
     }
 }
-
