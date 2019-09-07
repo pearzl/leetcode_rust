2cf28ec816c77b8c11e1c2a185fea8e46f019991
pearzl <302669696@qq.com>
2019-09-07 16:45:17 +0800

    fmt

--- src/answer/q0189_rotate_array.rs
+++ src/answer/q0189_rotate_array.rs
@@ -1,5 +1,4 @@
-// q0189_rotate_array 
-
+// q0189_rotate_array
 
 struct Solution;
 
@@ -7,12 +6,12 @@ impl Solution {
     pub fn rotate(nums: &mut Vec<i32>, k: i32) {
         let l = nums.len();
         if l == 0 {
-            return
+            return;
         }
         let k = k as usize % l;
-        let tmp = Vec::from(&nums[l-k..]);
+        let tmp = Vec::from(&nums[l - k..]);
         for i in (k..l).rev() {
-            nums[i] = nums[i-k];
+            nums[i] = nums[i - k];
         }
         for (i, v) in tmp.iter().enumerate() {
             nums[i] = *v;
@@ -20,21 +19,18 @@ impl Solution {
     }
 }
 
-
-
 #[cfg(test)]
 mod tests {
     use super::Solution;
 
     #[test]
     fn it_works() {
-        let mut v = vec![1,2,3,4,5,6,7];
+        let mut v = vec![1, 2, 3, 4, 5, 6, 7];
         Solution::rotate(&mut v, 3);
-        assert_eq!( vec![5,6,7,1,2,3,4], v);
+        assert_eq!(vec![5, 6, 7, 1, 2, 3, 4], v);
 
-        let mut v = vec![-1,-100,3,99];
+        let mut v = vec![-1, -100, 3, 99];
         Solution::rotate(&mut v, 2);
-        assert_eq!( vec![3,99,-1,-100], v);
+        assert_eq!(vec![3, 99, -1, -100], v);
     }
 }
-
